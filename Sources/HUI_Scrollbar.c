877052c64791f772d1dffcd8d973cf79bf5a1db2
Oherrou <olivier.herrou@insa-rennes.fr>
2015-06-28 20:20:07 +0200

    WIP Scrollbar
    Must clean and create SetCurrentValue

--- Sources/HUI_Scrollbar.c
+++ Sources/HUI_Scrollbar.c
@@ -23,21 +23,21 @@
  * \param  pSpriteScrollbar Pointer to the sprite used for the scrollbar.
  * \param  pSpriteSlider    Pointer to the sprite used for the slider.
  * \param  rHitboxScrollbar Hitbox of the scrollbar
- * \param  iMin             The minimum value of the scrollbar.
- * \param  iMax             The maximum value of the scrollbar (!= 0).
+ * \param  iMin             The minimum value of the scrollbar ( < iMax).
+ * \param  iMax             The maximum value of the scrollbar ( > iMin).
  * \return None.
  */
 void HUI_Scrollbar_Init(HUI_Scrollbar *pScrollbar, SDL_Sprite *pSpriteScrollbar, SDL_Sprite *pSpriteSlider, SDL_Rect rHitboxScrollbar, Sint32 iMin, Sint32 iMax)
 {
-    pScrollbar->iCurrent = 0;
-    pScrollbar->iMin     = iMin;
-    pScrollbar->iMax     = iMax;
+    pScrollbar->iCurrent = iMin;
+    pScrollbar->iMin = iMin;
+    pScrollbar->iMax = iMax;
 
 
     SDL_Anim_Init(&pScrollbar->sAnimScrollbar, pSpriteScrollbar);
     SDL_Anim_SetFrame(&pScrollbar->sAnimScrollbar, 0);
 
-    HUI_Button_Init(&pScrollbar->sButtonSlider, pSpriteSlider, 0, 0);
+    HUI_Button_Init(&pScrollbar->sButtonSlider, pSpriteSlider, rHitboxScrollbar.x, rHitboxScrollbar.y);
 
     pScrollbar->rHitboxScrollbar.x = rHitboxScrollbar.x;
     pScrollbar->rHitboxScrollbar.y = rHitboxScrollbar.y;
@@ -59,21 +59,25 @@ void HUI_Scrollbar_Init(HUI_Scrollbar *pScrollbar, SDL_Sprite *pSpriteScrollbar,
 */
 void HUI_Scrollbar_SetPosition(HUI_Scrollbar *pScrollbar, Sint32 x, Sint32 y)
 {
-    Uint32 iEcart = 0;
+    SDL_Point sShift;
+    SDL_Rect  rRectSlider;
 
-    iEcart = pScrollbar->sButtonSlider.sPointButton.x - pScrollbar->sPointScrollbar.x;
-    SDL_Anim_SetPosition(&pScrollbar->sAnimScrollbar, x, y); // ca va virer normalement
+    HUI_Button_GetHitbox(&pScrollbar->sButtonSlider, &rRectSlider);
 
+    sShift.x = pScrollbar->sButtonSlider.sPosition.x - pScrollbar->sPointScrollbar.x;
+ 
     pScrollbar->sPointScrollbar.x = x;
     pScrollbar->sPointScrollbar.y = y;
 
     pScrollbar->rHitboxScrollbar.x = x;
     pScrollbar->rHitboxScrollbar.y = y;
 
-    // TODO
-    // Update the slider position also without overwritting the current value 
-    HUI_Button_SetPosition(&pScrollbar->sButtonSlider, x +iEcart, y);
+    sShift.y = pScrollbar->sPointScrollbar.y + (pScrollbar->rHitboxScrollbar.h / 2) - (rRectSlider.h / 2);
+
+    HUI_Button_SetPosition(&pScrollbar->sButtonSlider, x + sShift.x, sShift.y);
 }
+
+
 /*!
 * \brief  Function to draw a scrollbar.
 *
@@ -82,7 +86,7 @@ void HUI_Scrollbar_SetPosition(HUI_Scrollbar *pScrollbar, Sint32 x, Sint32 y)
 */
 void HUI_Scrollbar_Draw(HUI_Scrollbar *pScrollbar)
 {
-    SDL_Anim_Draw(&pScrollbar->sAnimScrollbar);
+    SDL_Anim_Draw(&pScrollbar->sAnimScrollbar, &pScrollbar->sPointScrollbar);
     HUI_Button_Draw(&pScrollbar->sButtonSlider);
 }
 
@@ -98,32 +102,25 @@ void HUI_Scrollbar_Update(HUI_Scrollbar *pScrollbar, const HUI_Input *pInput)
     SDL_Rect  rRectSlider;
     SDL_Point sPointSlider;
     SDL_Point sMidSlider;
-    Uint32    iTmp = 0;
-    rRectSlider  = HUI_Button_GetHitbox(&pScrollbar->sButtonSlider);
-    sPointSlider = HUI_Button_GetPosition(&pScrollbar->sButtonSlider);
-    sMidSlider.x = rRectSlider.w / 2;
-    HUI_Button_Update(&pScrollbar->sButtonSlider,pInput);
+    Uint32    iPercentage = 0;
 
-    if(pScrollbar->sButtonSlider.bIsClicked) // Appuie
-    { 
+    HUI_Button_GetHitbox(&pScrollbar->sButtonSlider, &rRectSlider);
+    HUI_Button_GetPosition(&pScrollbar->sButtonSlider, &sPointSlider);
+
+    sMidSlider.x = rRectSlider.w / 2;
+    sMidSlider.y = rRectSlider.h / 2;
+
+    HUI_Button_Update(&pScrollbar->sButtonSlider, pInput);
+
+    if (HUI_Button_GetState(&pScrollbar->sButtonSlider) == HUI_BUTTON_ACTIVE) // Appuie
+    {
         if ((pInput->iMouse.x >= pScrollbar->sPointScrollbar.x) && (pInput->iMouse.x <= (pScrollbar->sPointScrollbar.x + pScrollbar->rHitboxScrollbar.w + sMidSlider.x)))  // Toujours dans la barre de scroll
         {
-            //Petit problème d'accroche de la barre si on bouge trop vite mais bon ...
-            
-
             if (pInput->iMouse.x - sMidSlider.x < 0) sMidSlider.x = 0; // empecher le curseur d'aller hors de la barre
-            HUI_Button_SetPosition(&pScrollbar->sButtonSlider, pInput->iMouse.x - sMidSlider.x, pScrollbar->sPointScrollbar.y);
+            HUI_Button_SetPosition(&pScrollbar->sButtonSlider, pInput->iMouse.x - sMidSlider.x , sPointSlider.y);
 
-            //Update current value
-            if ((sPointSlider.x == 0) || (sPointSlider.x - pScrollbar->sPointScrollbar.x) < 0)
-            {
-                pScrollbar->iCurrent = 0;
-            }
-            else
-            {
-                iTmp = pScrollbar->rHitboxScrollbar.w - (sPointSlider.x - pScrollbar->sPointScrollbar.x) - (1 / pScrollbar->rHitboxScrollbar.w);
-                pScrollbar->iCurrent = pScrollbar->iMax - (iTmp *pScrollbar->iMax) / pScrollbar->rHitboxScrollbar.w;
-            }
+            iPercentage = ((sPointSlider.x - pScrollbar->sPointScrollbar.x)*100 / (pScrollbar->sPointScrollbar.x + pScrollbar->rHitboxScrollbar.w - pScrollbar->sPointScrollbar.x));
+            pScrollbar->iCurrent = pScrollbar->iMin + (((pScrollbar->iMax - pScrollbar->iMin) * iPercentage) / 100);
             
         } 
     }
